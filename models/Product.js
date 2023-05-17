8ede63c50489f7d97e05b4e9c50e97d4045e7978
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-05-16 18:49:41 -0700

    add models/sequelize

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      // allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    price: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+      // prevents duplicate email addresses in DB
+    
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+  category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },    
   },
   {
     sequelize,
